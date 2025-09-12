111f648ca25854018cd6651fe6e58f3c6c40a636
Addy <goataadi07@gmail.com>
2025-09-12 17:38:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,59 +7,72 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+# Bot token @Botfather
+TG_BOT_TOKEN = "7966856816:AAEdC2-P7D_M9zRXk4vzU0B0y_433DtIRTU"
 
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+# Your API ID from my.telegram.org
+APP_ID = 25233517
 
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+# Your API Hash from my.telegram.org
+API_HASH = "ff87d34f51a644a4edc5a093997b3922"
 
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+# Your db channel Id
+CHANNEL_ID = -1002263661454
 
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+# OWNER ID
+OWNER_ID = 6150094366
 
-#Port
+# Port
 PORT = os.environ.get("PORT", "8080")
 
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+# Database
+DB_URI = "mongodb+srv://treclj_db_user:USRfscthOVAQBii7@cluster0.lqtky9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "AddyFileSharingBot"
 
-#force sub channel id, if you want enable force sub
+# force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start message
-START_PIC = os.environ.get("START_PIC","")
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+# start message
+START_PIC = os.environ.get("START_PIC", "")
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link."
+)
+
 try:
-    ADMINS=[]
+    ADMINS = []
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+# Force sub message
+FORCE_MSG = os.environ.get(
+    "FORCE_SUB_MESSAGE",
+    "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+)
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
+# set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-#set True if you want to prevent users from forwarding files from bot
+# set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 # Auto delete time in seconds.
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
-AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
-AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
+AUTO_DELETE_MSG = os.environ.get(
+    "AUTO_DELETE_MSG",
+    "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time."
+)
+AUTO_DEL_SUCCESS_MSG = os.environ.get(
+    "AUTO_DEL_SUCCESS_MSG",
+    "Your file has been successfully deleted. Thank you for using our service. ✅"
+)
 
-#Set true if you want Disable your Channel Posts Share button
+# Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
