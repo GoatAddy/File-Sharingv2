4e8ff4d384aa4e7f3c150d8ee8d899621b7605d1
Addy <goataadi07@gmail.com>
2025-09-12 17:47:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,83 +1,72 @@
 import os
+from os import environ,getenv
 import logging
-from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
-load_dotenv()
 
-# Bot token @Botfather
-TG_BOT_TOKEN = "7966856816:AAEdC2-P7D_M9zRXk4vzU0B0y_433DtIRTU"
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339")
+APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+#--------------------------------------------
 
-# Your API ID from my.telegram.org
-APP_ID = 25233517
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "colossals") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001")) # Owner id
+#--------------------------------------------
+PORT = os.environ.get("PORT", "8001")
+#--------------------------------------------
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
+#--------------------------------------------
+FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/colossals")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
+#--------------------------------------------
+START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/N69nBvYL/file-1922.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://i.ibb.co/jkWnPpD0/file-1924.jpg")
+#--------------------------------------------
 
-# Your API Hash from my.telegram.org
-API_HASH = "ff87d34f51a644a4edc5a093997b3922"
+HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ ᴀᴅᴅʏ\n\n❏ ᴀᴅᴍɪɴ/ᴄʜᴀɴɴᴇʟ\n├ᴀᴅᴍɪɴ : t.me/colossals\n├ᴄʜᴀɴɴᴇʟ : t.me/PythonPortalz\n└ɢʀᴏᴜᴘ : ʏᴇᴛ, ɴᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ\n\nᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ꜰᴏʀ ʙᴇꜱᴛ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ, ꜰʀᴇᴇ ᴘʏᴛʜᴏɴ ᴛᴏᴏʟꜱ!\n\n</blockquote></b>"
+ABOUT_TXT = "<b><blockquote>🤖 ʙᴏᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ\n\n◈ ɴᴀᴍᴇ : ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ\n◈ ᴠᴇʀsɪᴏɴ : 2.0\n◈ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ\n◈ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : ᴀᴅᴅʏ\n\nI ᴀᴍ ᴀ sɪᴍᴘʟᴇ ʙᴏᴛ ᴛʜᴀᴛ ʜᴇʟᴘs ʏᴏᴜ ᴛᴏ sᴛᴏʀᴇ ғɪʟᴇs ᴀɴᴅ ɢᴇɴᴇʀᴀᴛᴇ sʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋs.\n</blockquote></b>"
 
-# Your db channel Id
-CHANNEL_ID = -1002263661454
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ, {first}\n\n<blockquote> ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
 
-# OWNER ID
-OWNER_ID = 6150094366
+CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
 
-# Port
-PORT = os.environ.get("PORT", "8080")
+<b>›› /dlt_time :</b> sᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
+<b>›› /check_dlt_time :</b> ᴄʜᴇᴄᴋ ᴄᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
+<b>›› /dbroadcast :</b> ʙʀᴏᴀᴅᴄᴀsᴛ ᴅᴏᴄᴜᴍᴇɴᴛ / ᴠɪᴅᴇᴏ
+<b>›› /ban :</b> ʙᴀɴ ᴀ ᴜꜱᴇʀ
+<b>›› /unban :</b> ᴜɴʙᴀɴ ᴀ ᴜꜱᴇʀ
+<b>›› /banlist :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ʙᴀɴɴᴇᴅ ᴜsᴇʀs
+<b>›› /addchnl :</b> ᴀᴅᴅ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
+<b>›› /delchnl :</b> ʀᴇᴍᴏᴠᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
+<b>›› /listchnl :</b> ᴠɪᴇᴡ ᴀᴅᴅᴇᴅ ᴄʜᴀɴɴᴇʟs
+<b>›› /fsub_mode :</b> ᴛᴏɢɢʟᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴍᴏᴅᴇ
+<b>›› /pbroadcast :</b> sᴇɴᴅ ᴘʜᴏᴛᴏ ᴛᴏ ᴀʟʟ ᴜꜱᴇʀs
+<b>›› /add_admin :</b> ᴀᴅᴅ ᴀɴ ᴀᴅᴍɪɴ
+<b>›› /deladmin :</b> ʀᴇᴍᴏᴠᴇ ᴀɴ ᴀᴅᴍɪɴ
+<b>›› /admins :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ᴀᴅᴍɪɴs
+"""
 
-# Database
-DB_URI = "mongodb+srv://treclj_db_user:USRfscthOVAQBii7@cluster0.lqtky9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "AddyFileSharingBot"
-
-# force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002263661454"))
-JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", True)
-
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-# start message
-START_PIC = os.environ.get("START_PIC", "")
-START_MSG = os.environ.get(
-    "START_MESSAGE",
-    "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link."
-)
-
-try:
-    ADMINS = []
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-    raise Exception("Your Admins list does not contain valid integers.")
-
-# Force sub message
-FORCE_MSG = os.environ.get(
-    "FORCE_SUB_MESSAGE",
-    "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
-)
-
-# set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-# set True if you want to prevent users from forwarding files from bot
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @colossals</b>")
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-# Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
-AUTO_DELETE_MSG = os.environ.get(
-    "AUTO_DELETE_MSG",
-    "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time."
-)
-AUTO_DEL_SUCCESS_MSG = os.environ.get(
-    "AUTO_DEL_SUCCESS_MSG",
-    "Your file has been successfully deleted. Thank you for using our service. ✅"
-)
-
-# Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+BOT_STATS_TEXT = "ʙᴏᴛ ᴜᴘᴛɪᴍᴇ\n{uptime}"
+USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+# ✅ New Bot Settings Text
+BOT_SETTINGS_TEXT = """<b><blockquote>⚙️ ʙᴏᴛ sᴇᴛᴛɪɴɢs</blockquote>
+
+◈ ᴛᴏᴛᴀʟ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ: {total_channels}
+◈ ᴛᴏᴛᴀʟ ᴀᴅᴍɪɴs: {total_admins}
+◈ ᴛᴏᴛᴀʟ ʙᴀɴɴᴇᴅ ᴜsᴇʀs: {total_banned}
+◈ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴍᴏᴅᴇ: {autodel_time}
+◈ ᴘʀᴏᴛᴇᴄᴛ ᴄᴏɴᴛᴇɴᴛ: {protect_status}
+◈ ʀᴇǫᴜᴇsᴛ ғsᴜʙ ᴍᴏᴅᴇ: {fsub_mode}</b>
+"""
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -95,6 +84,7 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
